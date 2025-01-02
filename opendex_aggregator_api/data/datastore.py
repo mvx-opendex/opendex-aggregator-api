7da9ed419ae760a817124459a7d7568020d6720a
Aurélien Passion <aurelien.passion@gmail.com>
2025-01-02 14:09:04 +0100

    implement local memory cache for datastore

--- opendex_aggregator_api/data/datastore.py
+++ opendex_aggregator_api/data/datastore.py
@@ -1,8 +1,11 @@
 
 import base64
+import logging
 import pickle
 from datetime import timedelta
 from typing import List, Optional
+
+from cachetools import TTLCache, cached
 
 from opendex_aggregator_api.data.model import Esdt, ExchangeRate
 from opendex_aggregator_api.pools.model import SwapPool
@@ -10,6 +13,7 @@ from opendex_aggregator_api.pools.pools import AbstractPool
 from opendex_aggregator_api.utils.redis_utils import redis_get, redis_set
 
 
+@cached(cache=TTLCache(maxsize=1, ttl=10))
 def get_swap_pools() -> List[SwapPool]:
     return redis_get('swap_pools',
                      lambda json_: [SwapPool.model_validate_json(x) for x in json_])
@@ -36,7 +40,9 @@ def set_dex_aggregator_pool(sc_address: str, token_in: str, token_out: str, pool
               timedelta(seconds=60))
 
 
+@cached(cache=TTLCache(maxsize=1, ttl=10))
 def get_tokens() -> Optional[List[Esdt]]:
+    logging.info('load tokens from datastore')
     return redis_get('tokens',
                      lambda json_: [Esdt.model_validate(x) for x in json_])
 
