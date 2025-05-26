d621c9bcd06238f217a499dec96e6c88172aaf61
Aurélien Passion <aurelien.passion@gmail.com>
2025-05-26 17:52:49 +0200

    xoxno: at least 1 EGLD to stake

--- opendex_aggregator_api/pools/xoxno.py
+++ opendex_aggregator_api/pools/xoxno.py
@@ -1,12 +1,22 @@
 from dataclasses import dataclass
+from typing import Tuple
 
 from typing_extensions import override
 
+from opendex_aggregator_api.data.model import Esdt
 from opendex_aggregator_api.pools.pools import ConstantPricePool
+from opendex_aggregator_api.services.tokens import WEGLD_IDENTIFIER
 
 
 @dataclass
 class XoxnoConstantPricePool(ConstantPricePool):
+
+    @override
+    def estimate_amount_out(self, token_in: Esdt, amount_in: int, token_out: Esdt) -> Tuple[int, int, int]:
+        if token_in.identifier == WEGLD_IDENTIFIER and amount_in < 1_000_000_000_000_000_000:
+            raise ValueError("Amount in must be greater than 1 ESDT")
+
+        return super().estimate_amount_out(token_in, amount_in, token_out)
 
     @override
     def deep_copy(self):
